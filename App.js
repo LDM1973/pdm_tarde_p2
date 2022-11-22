aecd174504bb17df9f2b680131204dd11ecb3409
Atalla Said <atallasaid@gmail.com>
2022-11-22 10:11:51 -0300

    Adição de arquivos

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import PessoaAddTela from './telas/PessoaAddTela'
+import PessoaListTela from './telas/PessoaListTela';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+const Stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='PessoaList'>
+        <Stack.Screen
+          name="PessoaAdd"
+          component={PessoaAddTela}
+        />
+        <Stack.Screen
+          name='PessoaList'
+          component={PessoaListTela} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
